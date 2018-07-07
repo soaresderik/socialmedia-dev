0f3a49e652c22b3412d4b32197f38e99bd799229
Derik Soares <codigoaberto.info@gmail.com>
2018-07-06 23:01:27 -0300

    Adicionando e configurando redux

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+
+    const user = {
+      email,
+      password
+    };
+
+    console.log(user);
+  }
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Entrar</h1>
+              <p className="lead text-center">
+                Entre com sua conta do DevConnector
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="E-mail"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                    name="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
